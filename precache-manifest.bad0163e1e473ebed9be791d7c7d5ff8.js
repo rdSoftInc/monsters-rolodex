7d594b7933254037a7aceae56a073a918eb95f9d
rolstan dsouza <rolstandsouza@gmail.com>
2020-02-22 21:24:44 +0530

    Updates

---
+++ precache-manifest.bad0163e1e473ebed9be791d7c7d5ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ac6b3523bdcfdfd206accf489891c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a9b153a1f77a10063b0",
+    "url": "/monsters-rolodex/static/css/main.2437abd6.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9b153a1f77a10063b0",
+    "url": "/monsters-rolodex/static/js/main.23826f55.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
